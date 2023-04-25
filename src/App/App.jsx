100e2f41cc2cb98a9d7952339e05041d46fa2abd
yabr87 <108272773+yabr87@users.noreply.github.com>
2023-04-26 01:43:36 +0300

    start react app

---
+++ src/App/App.jsx
@@ -0,0 +1,16 @@
+import { Container, AppHeader } from './App.styles';
+
+function App() {
+  return (
+    <>
+      <AppHeader>
+        <p>some header</p>
+      </AppHeader>
+      <Container>
+        <p>some text</p>
+      </Container>
+    </>
+  );
+}
+
+export default App;
---
+++ src/App/App.styles.js
@@ -0,0 +1,16 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const AppHeader = styled.div`
+  display: flex;
+
+  align-items: center;
+  justify-content: center;
+  background-color: #ddd;
+`;
