f8852d98a6c3d7bec6160638fa0c5c6f3ac654ea
yabr87 <108272773+yabr87@users.noreply.github.com>
2023-06-06 22:58:34 +0300

    test5

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter>
+  <BrowserRouter basename="best-it-job-aggregator-ever">
     <React.StrictMode>
       <App />
     </React.StrictMode>
